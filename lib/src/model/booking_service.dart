23c29d5429c679392052b3a211e4de79d91ea9e1
radikris <radi.kristof@gmail.com>
2022-01-05 22:53:45 +0100

    added comments for the booking service model

--- lib/src/model/booking_service.dart
+++ lib/src/model/booking_service.dart
@@ -4,15 +4,43 @@ part 'booking_service.g.dart';
 @JsonSerializable(explicitToJson: true)
 class BookingService {
   /// The generated code assumes these values exist in JSON.
+  ///
+  /// The userId of the currently logged user
+  /// who will start the new booking
   final String? userId;
+
+  /// The userName of the currently logged user
+  /// who will start the new booking
   final String? userName;
+
+  /// The userEmail of the currently logged user
+  /// who will start the new booking
   final String? userEmail;
+
+  /// The userPhoneNumber of the currently logged user
+  /// who will start the new booking
   final String? userPhoneNumber;
+
+  /// The id of the currently selected Service
+  /// for this service will the user start the new booking
+
   final String? serviceId;
+
+  ///The name of the currently selected Service
   final String serviceName;
+
+  ///The duration of the currently selected Service
+
   final int serviceDuration;
+
+  ///The price of the currently selected Service
+
   final int? servicePrice;
+
+  ///The selected booking slot's starting time
   DateTime bookingStart;
+
+  ///The selected booking slot's ending time
   DateTime bookingEnd;
 
   BookingService(
