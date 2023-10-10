7207a2228b32b651d7de19ecc13e1c3eb3929e36
Muslimin Ontong <moshOntong@gmail.com>
2023-10-10 21:05:30 +0800

    feat: 1.3.2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -39,7 +39,7 @@ class _BookingCalendarDemoAppState
 
     mockBookingController = BookingController(
         serviceOpening: DateTime(2023, 8, 18, 8, 0), // 08:00 AM
-        serviceClosing: DateTime(2023, 8, 18, 17, 0), // 05:00 PM
+        serviceClosing: DateTime(2023, 8, 18, 20, 0), // 05:00 PM
 
         bookingService: mockBookingService,
         pauseSlots: generatePauseSlots());
@@ -47,7 +47,23 @@ class _BookingCalendarDemoAppState
 
   Stream<dynamic>? getBookingStreamMock(
       {required DateTime end, required DateTime start}) {
-    return Stream.value([]);
+    // Mock data
+    final bookings = [
+      {
+        'bookingStart': "2023-08-28T10:00:00.000+08:00",
+        'bookingEnd':
+            "2023-08-28T11:00:00.000+08:00", // Just an example for the end time
+      },
+
+      // ... add more mock bookings as needed
+      {
+        'bookingStart': "2023-08-28T12:00:00.000+00:00",
+        'bookingEnd':
+            "2023-08-28T13:00:00.000+00:00", // Just an example for the end time
+      }
+    ];
+
+    return Stream.value(bookings);
   }
 
   Future<dynamic> onBookSelected(BuildContext context,
@@ -82,27 +98,35 @@ class _BookingCalendarDemoAppState
   List<DateTimeRange> converted = [];
 
   List<DateTimeRange> convertStreamResultMock({required dynamic streamResult}) {
+    for (var booking in streamResult) {
+      DateTime start = DateTime.parse(booking['bookingStart'] as String);
+      DateTime end = DateTime.parse(booking['bookingEnd'] as String);
+      converted.add(DateTimeRange(start: start, end: end));
+    }
+
+    return converted;
+
     ///here you can parse the streamresult and convert to [List<DateTimeRange>]
     ///take care this is only mock, so if you add today as disabledDays it will still be visible on the first load
     ///disabledDays will properly work with real data
-    DateTime first = now;
-    DateTime tomorrow = now.add(Duration(days: 1));
-    DateTime second = now.add(const Duration(minutes: 55));
-    DateTime third = now.subtract(const Duration(minutes: 240));
-    DateTime fourth = now.subtract(const Duration(minutes: 500));
-    converted.add(
-        DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
-    converted.add(DateTimeRange(
-        start: second, end: second.add(const Duration(minutes: 23))));
-    converted.add(DateTimeRange(
-        start: third, end: third.add(const Duration(minutes: 15))));
-    converted.add(DateTimeRange(
-        start: fourth, end: fourth.add(const Duration(minutes: 50))));
+    // DateTime first = now;
+    // DateTime tomorrow = now.add(Duration(days: 1));
+    // DateTime second = now.add(const Duration(minutes: 55));
+    // DateTime third = now.subtract(const Duration(minutes: 240));
+    // DateTime fourth = now.subtract(const Duration(minutes: 500));
+    // converted.add(
+    //     DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
+    // converted.add(DateTimeRange(
+    //     start: second, end: second.add(const Duration(minutes: 23))));
+    // converted.add(DateTimeRange(
+    //     start: third, end: third.add(const Duration(minutes: 15))));
+    // converted.add(DateTimeRange(
+    //     start: fourth, end: fourth.add(const Duration(minutes: 50))));
 
-    //book whole day example
-    converted.add(DateTimeRange(
-        start: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 5, 0),
-        end: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 23, 0)));
+    // //book whole day example
+    // converted.add(DateTimeRange(
+    //     start: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 5, 0),
+    //     end: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 23, 0)));
     return converted;
   }
 
@@ -195,12 +219,12 @@ class _BookingCalendarDemoAppState
                 hideBreakTime: false,
                 loadingWidget: const Text('Fetching data...'),
                 // uploadingWidget: const CircularProgressIndicator(),
-                locale: 'hu_HU',
+                // locale: 'hu_HU',
                 startingDayOfWeek: StartingDayOfWeek.tuesday,
                 wholeDayIsBookedWidget:
                     const Text('Sorry, for this day everything is booked'),
                 //disabledDates: [DateTime(2023, 1, 20)],
-                //disabledDays: [6, 7],
+                disabledDays: [6, 7],
               ),
             ),
           );
